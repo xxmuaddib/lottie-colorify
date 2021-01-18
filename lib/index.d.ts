7e25e7242e5ebbb9d83b4827494ef70b795dd23f
Mikayel Saghyan <xxmuaddib@gmail.com>
2021-01-18 10:57:55 +0400

    Add lib folder to git for example to work

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+export declare const colorify: (destColors: (string | number[] | undefined)[] | undefined, lottie: any) => any;
+export declare const replaceColor: (sourceColor: string | number[], targetColor: string | number[], lottieObj: any) => any;
+export declare const flatten: (targetColor: string | number[], lottieObj: any) => any;
+export declare const getColors: (lottieObj: any) => any;
